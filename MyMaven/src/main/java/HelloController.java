cb8dfe38602ac59f799c5378d0034c34b23d0f2a
chh-hub <961959018@qq.com>
2019-09-25 15:47:32 +0800

    maven程序示例

--- MyMaven/src/main/java/HelloController.java
+++ MyMaven/src/main/java/HelloController.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+package com.hunau.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "陈航 201841882115";
+    }
 }
